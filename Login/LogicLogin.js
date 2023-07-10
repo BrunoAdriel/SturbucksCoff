481c4cc61b9aa51f5396d518e09e69c617eefcca
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2023-07-10 19:20:54 -0300

    register

--- Login/LogicLogin.js
+++ Login/LogicLogin.js
@@ -1,12 +1,12 @@
 //Pagina de Registro 
 
-let nombre = document.getElementById("Name");
-let apellido = document.getElementById("Surname")
-let contraseña1 = document.getElementById("Pass1");
-let contraseña2 = document.getElementById("Pass2");
-let email = document.getElementById("Email");
-let registro = document.getElementById("registrarse")
-let nuevosUsuarios = [];
+// let nombre = document.getElementById("Name");
+// let apellido = document.getElementById("Surname")
+// let contraseña1 = document.getElementById("Pass1");
+// let contraseña2 = document.getElementById("Pass2");
+// let email = document.getElementById("Email");
+// let registro = document.getElementById("registrarse")
+// let nuevosUsuarios = [];
 
 //Funciones
 
@@ -19,8 +19,3 @@ let nuevosUsuarios = [];
 
 
 
-// function usuarioCreado(e){
-//     nuevosUsuarios.push(nombre, apellido, contraseña1, contraseña2, email)
-//     localStorage.setItem('usuraiosGuardados', JSON.stringify(nuevosUsuarios));
-
-// }
--- Login/RegisterLogic.js
+++ Login/RegisterLogic.js
@@ -1,21 +1,21 @@
 
-const registerForm = document.querySelector('#registerForm')
+const registerForm = document.querySelector('#registerForm');
 
 registerForm.addEventListener('submit',(e)=>{
-    e.preventDefault()
-    const nombre = document.querySelector('#nombre').value
-    const apellido = document.querySelector('#apellido').value
-    const email = document.querySelector('#email').value
-    const contraseña = document.querySelector('#contraseña').value
+    e.preventDefault();
+    const nombre = document.querySelector('#nombre').value;
+    const apellido = document.querySelector('#apellido').value;
+    const email = document.querySelector('#email').value;
+    const contrasenia = document.querySelector('#contrasenia').value;
 
     const usuarios = JSON.parse(localStorage.getItem('usuariosNuevos')) || [];
     const usuarioRegistrado = usuarios.find (e => e.email === email );
     if(usuarioRegistrado){
-        return alert('Este email ya esta registrado')
+        return alert('Este email ya esta registrado');
     }
 
 
-    usuarios.push({nombre: nombre, apellido: apellido, email: email, contraseña: contraseña})
+    usuarios.push({nombre: nombre, apellido: apellido, email: email, contrasenia: contrasenia})
     localStorage.setItem('usuariosNuevos', JSON.stringify(usuarios))
-    alert('Registro Exitoso!!')
+    alert('Registro Exitoso!!');
 })
