ecba120b8fbaa77a0f18f56519f324b49b5a9b49
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2023-07-10 21:50:37 -0300

    arreglo registro

--- Login/RegisterLogic.js
+++ Login/RegisterLogic.js
@@ -1,24 +1,28 @@
-
+//vinculo el formulario al JS
 const registerForm = document.querySelector('#registerForm');
 
+//obtencion de datos y guardado en local
 registerForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     const nombre = document.querySelector('#nombre').value;
-    const apellido = document.querySelector('#apellido').value;
     const email = document.querySelector('#email').value;
     const contrasenia = document.querySelector('#contrasenia').value;
+    const contraseniaConfirm = document.querySelector('#contraseniaConfirm').value;
 
     const usuarios = JSON.parse(localStorage.getItem('usuariosNuevos')) || [];
     const usuarioRegistrado = usuarios.find (e => e.email === email );
     if(usuarioRegistrado){
         return alert('Este email ya esta registrado');
-    }else{
+    } else if(contrasenia != contraseniaConfirm){
+        return alert('las contraseñas no coindicen!');
+    } 
+    else{
         alert('Registro Exitoso!! Te llevaremos a la pagina de Ingreso.');
         window.location.href = 'Login.html'
     }
 
-
-    usuarios.push({nombre: nombre, apellido: apellido, email: email, contrasenia: contrasenia})
+//subida de datos al array en el caso de que se cumplan las condiciones
+    usuarios.push({nombre: nombre, email: email, contrasenia: contrasenia, contraseniaConfirm: contraseniaConfirm})
     localStorage.setItem('usuariosNuevos', JSON.stringify(usuarios))
 
 })
